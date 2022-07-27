a9c604461b8e38c0fdab2f27c3a5d4c6710180e1
sharsidd86 <110119641+sharsidd86@users.noreply.github.com>
2022-07-27 10:10:43 -0700

    Update aws-params.js

--- src/aws-params.js
+++ src/aws-params.js
@@ -8,13 +8,13 @@ const awsparams = {
     "aws_cloud_logic_custom": [
         {
             "name": "AmplifyWebApp",
-            "endpoint": "Value for your API Gateway Endpoint",
+            "endpoint": "https://cc85okan22.execute-api.us-east-1.amazonaws.com/dev",
             "region": "us-east-1"
         }
     ],
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "Value for your Cognito User Pool ID",
-    "aws_user_pools_web_client_id": "Value for your Cognito Web Client ID",
+    "aws_user_pools_id": "us-east-1_TxhMTM0FR",
+    "aws_user_pools_web_client_id": "47q2op57l07pnr3i0esk86qs17",
     "oauth": {},
     "app_linking": {
         "COGNITO_AUTH_URL": 'Value for your Cognito Authorization URL',
